e380fb218737bfde4e6f6cfc04f86be37bdc64c2
arun.kumar <arun.kumar@smartbox.com>
2020-01-27 14:39:06 +0000

    added sample stub for availability and price

---
+++ stub/sandbox/data/property.js
@@ -0,0 +1,50 @@
+exports.property = {
+    "id": "11139b50-7c60-11e9-b5aa-0242ac130002",
+    "partner": {
+        "id": "4f281a4c-6b44-11e9-8030-0242ac120003",
+        "externalId": "test1558508761294",
+        "password": "test123",
+        "roles": [
+            "ROLE_PARTNER"
+        ],
+        "status": "disabled",
+        "salt": null,
+        "createdAt": "2019-05-22T08:06:01+01:00",
+        "updatedAt": "2019-05-22T08:06:01+01:00"
+    },
+    "name": "Ibis hotel",
+    "rooms": [
+        {
+            "id": "11",
+            "title": "single room",
+            "quantity": 1,
+            "createdAt": "2019-05-22T08:06:08+01:00",
+            "updatedAt": "2019-05-22T08:06:08+01:00",
+            "sellable": "disabled"
+        }, {
+            "id": "22",
+            "title": "double room",
+            "quantity": 1,
+            "createdAt": "2019-05-22T08:06:08+01:00",
+            "updatedAt": "2019-05-22T08:06:08+01:00",
+            "sellable": "enabled"
+        }, {
+            "id": "33",
+            "title": "triple room",
+            "quantity": 1,
+            "createdAt": "2019-05-22T08:06:08+01:00",
+            "updatedAt": "2019-05-22T08:06:08+01:00",
+            "sellable": "enabled"
+        }, {
+            "id": "44",
+            "title": "deluxe room",
+            "quantity": 1,
+            "createdAt": "2019-05-22T08:06:08+01:00",
+            "updatedAt": "2019-05-22T08:06:08+01:00",
+            "sellable": "disabled"
+        },
+    ],
+    "createdAt": "2019-05-22T08:06:05+01:00",
+    "updatedAt": "2019-05-22T08:06:05+01:00",
+    "currencyCode": "EUR",
+}
---
+++ stub/sandbox/main.js
@@ -0,0 +1,100 @@
+var moment = require('helpers/moment.min.js');
+state.property = require("./data/property.js").property;
+state.availabilities = state.availabilities || [];
+state.prices = state.prices || [];
+
+//set Data in state
+ setDataInState();
+
+Sandbox.define('/api/room_availabilities', 'OPTIONS', function (req, res) {
+    res.set('Access-Control-Allow-Origin', '*');
+});
+
+Sandbox.define('/api/room_availabilities', 'GET', function (req, res) {
+    // Check the request, make sure it is a compatible type
+    res.set('Access-Control-Allow-Origin', '*');
+    if (!req.is('application/json')) {
+        //return res.send(400, 'Invalid content type, expected application/json');
+    }
+
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+
+    // Set the status code of the response.
+    res.status(200);
+
+    // Send the response body.
+    var response = buildResponse();
+
+    res.json(response);
+
+    function buildResponse() {
+        var responseArray = [];
+        var numberOfDays = calculateDaysBetweenStartDateAndEndDate(req.query.startDate, req.query.endDate);
+        var from = moment(req.query.startDate, 'YYYY-MM-DD');
+        for (var i = 0; i <= numberOfDays; i++) {
+            if (state.availabilities[req.query.roomId][from.clone().add(i, 'days').format('YYYY-MM-DD')]) {
+                responseArray.push(state.availabilities[req.query.roomId][from.clone().add(i, 'days').format('YYYY-MM-DD')]);
+            }
+        }
+        return responseArray;
+    }
+});
+
+Sandbox.define('/api/room_prices', 'OPTIONS', function (req, res) {
+    res.set('Access-Control-Allow-Origin', '*');
+});
+
+Sandbox.define('/api/room_prices', 'GET', function (req, res) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.status(200);
+    var responseArray = [];
+    var numberOfDays = calculateDaysBetweenStartDateAndEndDate(req.query.startDate, req.query.endDate);
+    var from = moment(req.query.startDate, 'YYYY-MM-DD');
+    for (var i = 0; i <= numberOfDays; i++) {
+        if (state.prices[req.query.roomId][from.clone().add(i, 'days').format('YYYY-MM-DD')]) {
+            responseArray.push(state.prices[req.query.roomId][from.clone().add(i, 'days').format('YYYY-MM-DD')]);
+        }
+    }
+    return res.json(responseArray);
+});
+
+
+function calculateDaysBetweenStartDateAndEndDate(start, end) {
+    var from = moment(start, 'YYYY-MM-DD');
+    var to = moment(end, 'YYYY-MM-DD');
+    return to.diff(from, 'days');
+}
+function setDataInState()
+{
+// populate state of availability and prices of each room if needed
+state.property.rooms.forEach(function (room) {
+    if (!state.availabilities[room.id]) {
+        state.availabilities[room.id] = [];
+        for (var i = 0; i < 60; i++) {
+            var newDate = moment().add(i, 'days').format('YYYY-MM-DD');
+            state.availabilities[room.id][newDate] = {
+                date: newDate,
+                partner_golden_id: 'partner'+ i,
+                room_golden_id: 'room' + i,
+                rate_band_golden_id: 'rate' + i,
+                stock: 5,
+            };
+        }
+    }
+    if (!state.prices[room.id]) {
+        state.prices[room.id] = [];
+        for (var i = 0; i < 60; i++) {
+            var newDate = moment().add(i, 'days').format('YYYY-MM-DD');
+            state.prices[room.id][newDate] = {
+                date: newDate,
+                partner_golden_id: 'partner'+ i,
+                room_golden_id: 'room' + i,
+                rate_band_golden_id: 'rate' + i,
+                price: 67.90 + i,
+                currency: 'EUR'
+            }
+        }
+    }
+});
+}
