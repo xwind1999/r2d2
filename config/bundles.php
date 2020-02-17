076b6fd5a009a0bd8a8ac81361eb8bfa3f5ad672
Joao Jacome <joao.jacome@smartbox.com>
2020-02-17 11:45:37 +0000

    [R2D2-90] Added LiipMonitorBundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Liip\MonitorBundle\LiipMonitorBundle::class => ['all' => true],
 ];
