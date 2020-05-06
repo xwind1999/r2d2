8dd23cdb44472445d63372454acd2c1352539608
Joao Jacome <joao.jacome@smartbox.com>
2020-05-06 21:10:11 +0100

    [R2D2-128] Fixes for log messages

--- src/Exception/Resolver/UnprocessableProductRelationshipTypeException.php
+++ src/Exception/Resolver/UnprocessableProductRelationshipTypeException.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\Exception\Resolver;
 
 use App\Exception\ContextualException;
+use Symfony\Component\Messenger\Exception\UnrecoverableExceptionInterface;
 
-class UnprocessableProductRelationshipTypeException extends ContextualException
+class UnprocessableProductRelationshipTypeException extends ContextualException implements UnrecoverableExceptionInterface
 {
     protected const CODE = 1100002;
     protected const MESSAGE = 'Unprocessable product relationship type';
--- src/Exception/Resolver/UnprocessableProductTypeException.php
+++ src/Exception/Resolver/UnprocessableProductTypeException.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\Exception\Resolver;
 
 use App\Exception\ContextualException;
+use Symfony\Component\Messenger\Exception\UnrecoverableExceptionInterface;
 
-class UnprocessableProductTypeException extends ContextualException
+class UnprocessableProductTypeException extends ContextualException implements UnrecoverableExceptionInterface
 {
     protected const CODE = 1100001;
     protected const MESSAGE = 'Unprocessable product type';
