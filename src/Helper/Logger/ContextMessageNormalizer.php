2e02b330538e3a8ad0dc793b868aef5e16a70827
Joao Jacome <joao.jacome@smartbox.com>
2020-05-06 16:16:57 +0100

    Fixing some logs reusing string fields to log a class by Symfony

---
+++ src/Helper/Logger/ContextMessageNormalizer.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper\Logger;
+
+use Clogger\MiddlewareInterface;
+
+class ContextMessageNormalizer implements MiddlewareInterface
+{
+    /**
+     * @param mixed $level
+     * @param mixed $message
+     */
+    public function process($level, $message, array $context): array
+    {
+        if (isset($context['message']) && !is_string($context['message'])) {
+            $context['message_parsed'] = $context['message'];
+            unset($context['message']);
+        }
+
+        return [$message, $context];
+    }
+}
