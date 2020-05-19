b18d34e5b3e79dba730144a58ccbb4eae0a97cba
Uday.Chitturi <uday.chitturi@smartbox.com>
2020-05-19 17:55:05 +0100

    Updated mock with CMHUB availability and price broadcast calls

--- stub/sandbox/main.js
+++ stub/sandbox/main.js
@@ -1,16 +1,25 @@
 var moment = require('helpers/moment.min.js');
 state.property = require("./data/property.js").property;
+state.product = require("./data/product.js").property;
 state.availabilities = state.availabilities || [];
+state.cmhubavailabilities = state.cmhubavailabilities || [];
 state.prices = state.prices || [];
 state.booking = state.booking || [];
+state.box = state.box || [];
+state.experience = state.experience || [];
+state.component = state.component || [];
+state.box_experience = state.box_experience || [];
+state.experience_component = state.experience_component || [];
+state.experience_price = state.experience_price || [];
 
 //set Data in state
 setDataInState();
+setAvailabilityDataInState();
 
-Sandbox.define('/api/room_availabilities', 'OPTIONS', function (req, res) {
+/*Sandbox.define('/api/room_availabilities', 'OPTIONS', function (req, res) {
     res.set('Access-Control-Allow-Origin', '*');
 });
-
+*/
 Sandbox.define('/api/room_availabilities', 'GET', function (req, res) {
     // Check the request, make sure it is a compatible type
     res.set('Access-Control-Allow-Origin', '*');
@@ -42,7 +51,7 @@ Sandbox.define('/api/room_availabilities', 'GET', function (req, res) {
     }
 });
 
-Sandbox.define('/broadcast-listeners/partner', 'POST', function(req, res){
+Sandbox.define('/broadcast-listener/partner', 'POST', function(req, res){
 
     state.partner = state.partner || [];
     //validate request from EAI about partner information
@@ -81,11 +90,7 @@ Sandbox.define('/broadcast-listeners/partner', 'POST', function(req, res){
     })
 });
 
-Sandbox.define('/broadcast-listeners/product', 'POST', function(req, res) {
-
-    state.box = state.box || [];
-    state.experience = state.experience || [];
-    state.component = state.component || [];
+Sandbox.define('/broadcast-listener/product', 'POST', function(req, res) {
 
     // validate request from EAI about Product information
     if (req.body.id === undefined) {
@@ -135,10 +140,7 @@ Sandbox.define('/broadcast-listeners/product', 'POST', function(req, res) {
     })
 });
 
-Sandbox.define('/broadcast-listeners/product-relationship','POST',function (req, res) {
-
-    state.box_experience = state.box_experience || [];
-    state.experience_component = state.experience_component || [];
+Sandbox.define('/broadcast-listener/product-relationship','POST',function (req, res) {
 
 
     // validate request from EAI about Product-relationship
@@ -184,6 +186,30 @@ Sandbox.define('/broadcast-listeners/product-relationship','POST',function (req,
 
     return res.json(202, {
         status: "Relationship handled"
+    })
+});
+
+Sandbox.define('/broadcast-listener/price-information','POST',function (req, res) {
+
+    // validate request from EAI about Price Information
+    if (req.body.product === undefined) {
+        return res.json(422, {
+            status: "Error: Unprocessable Entity",
+            details: "product : This field cant be null"
+        })
+    }
+
+    if(!req.is('application/json'))
+    {
+        return res.json(400, {
+            details: "Invalid Content type, expected application/json"
+        })
+    }
+
+    state.experience_price.push(req.body)
+
+    return res.json(202, {
+        status: "Price information handled"
     })
 });
 
@@ -300,6 +326,60 @@ Sandbox.define('/api/booking','PATCH',function (req, res) {
     })
 })
 
+// CMHUB Get Availability
+Sandbox.define('/api/availability','GET',function (req, res) {
+
+    if(!req.is('application/json'))
+    {
+        return res.json(400, {
+            details: "Invalid Content type, expected application/json"
+        })
+    }
+
+    if(req.query.productId === undefined) {
+        return res.json(422, {
+            status: "Error: Unprocessable Entity",
+            details: "ProductID : This field cant be null"
+        })
+    }
+
+    if(req.query.start === undefined) {
+        return res.json(422, {
+            status: "Error: Unprocessable Entity",
+            details: "start : This value should not be blank"
+        })
+    }
+
+    if(req.query.end === undefined) {
+        return res.json(422, {
+            status: "Error: Unprocessable Entity",
+            details: "end : This value should not be blank"
+        })
+    }
+
+
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+
+    res.status(200);
+
+    var response = Response();
+    res.json(response)
+
+    function Response() {
+        var responseArray = [];
+        var numberOfDays = calculateDaysBetweenStartDateAndEndDate(req.query.start, req.query.end);
+        var from = moment(req.query.start, 'YYYY-MM-DD');
+        for (var i = 0; i <= numberOfDays; i++) {
+            if (state.cmhubavailabilities[req.query.productId][from.clone().add(i, 'days').format('YYYY-MM-DD')]) {
+                responseArray.push(state.cmhubavailabilities[req.query.productId][from.clone().add(i, 'days').format('YYYY-MM-DD')]);
+            }
+        }
+        return responseArray;
+    }
+
+})
+
 /*Sandbox.define('/api/room_prices', 'GET', function (req, res) {
     res.set('Access-Control-Allow-Origin', '*');
     res.status(200);
@@ -315,8 +395,6 @@ Sandbox.define('/api/booking','PATCH',function (req, res) {
 });Sandbox.define('/api/room_prices', 'OPTIONS', function (req, res) {
     res.set('Access-Control-Allow-Origin', '*');
 });*/
-
-
 
 function calculateDaysBetweenStartDateAndEndDate(start, end) {
     var from = moment(start, 'YYYY-MM-DD');
@@ -352,6 +430,23 @@ function setDataInState()
                     price: 67.90 + i,
                     currency: 'EUR'
                 }
+            }
+        }
+    });
+}
+
+function setAvailabilityDataInState()
+{
+// populate state of availability and prices of each room if needed
+    state.product.products.forEach(function (product) {
+        if (!state.cmhubavailabilities[product.id]) {
+            state.cmhubavailabilities[product.id] = [];
+            for (var i = 0; i < 60; i++) {
+                var newDate = moment().add(i, 'days').format('YYYY-MM-DD');
+                state.cmhubavailabilities[product.id][newDate] = {
+                    date: newDate,
+                    quantity: 40
+                };
             }
         }
     });
