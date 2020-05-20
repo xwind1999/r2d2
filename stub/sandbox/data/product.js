080a6102feb65d7f4088e8c59438af84926cbb76
Uday.Chitturi <uday.chitturi@smartbox.com>
2020-05-20 11:16:03 +0100

    Added new js file product for Get Availability call

---
+++ stub/sandbox/data/product.js
@@ -0,0 +1,14 @@
+exports.property = {
+    "products": [
+        {
+            "id": "286201",
+            "title" : "Multi package 1"
+        }, {
+            "id": "286202",
+            "title" : "Multi package 2"
+        }, {
+            "id": "286203",
+            "title" : "Multi package 3"
+        }
+    ],
+}
