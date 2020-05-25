b4f160aee713e5246a4befe4484860f5df1c0f2d
Joao Jacome <joao.jacome@smartbox.com>
2020-05-25 18:08:42 +0100

    limiting number of records processed per worker before restarting it

--- bin/start-command.sh
+++ bin/start-command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -s /etc/sv/sym /service/sym
 
-export COMMAND='/app/bin/console messenger:consume broadcast-listeners-partner broadcast-listeners-product broadcast-listeners-product-relationship broadcast-listeners-price-information'
+export COMMAND='/app/bin/console messenger:consume broadcast-listeners-partner broadcast-listeners-product broadcast-listeners-product-relationship broadcast-listeners-price-information --limit=10 --time-limit=3600'
 
 /app/bin/warm-up.sh
 
