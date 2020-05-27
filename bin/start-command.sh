1641577db6a331e15f29f1c24149c0caf6150252
Joao Jacome <joao.jacome@smartbox.com>
2020-05-27 17:51:17 +0100

    lowering worker time limit and removing record limitr

--- bin/start-command.sh
+++ bin/start-command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -s /etc/sv/sym /service/sym
 
-export COMMAND='/app/bin/console messenger:consume broadcast-listeners-partner broadcast-listeners-product broadcast-listeners-product-relationship broadcast-listeners-price-information --limit=10 --time-limit=3600'
+export COMMAND='/app/bin/console messenger:consume broadcast-listeners-partner broadcast-listeners-product broadcast-listeners-product-relationship broadcast-listeners-price-information --time-limit=300'
 
 /app/bin/warm-up.sh
 
