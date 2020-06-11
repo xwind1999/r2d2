a0d4aa1a61c4f31982729928bbd95a14016408dd
Bruno Henrique de Souza <bruno.souza@smartbox.com>
2020-06-11 14:14:45 +0100

    R2D2-228: Implementing Correlation-id in request and response events

---
+++ src/Http/CorrelationId/CorrelationId.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\CorrelationId;
+
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class CorrelationId
+{
+    public const HEADER_KEY = 'Correlation-Id';
+
+    private string $uuid;
+
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+        $this->uuid = $this->generateUuid();
+    }
+
+    protected function generateUuid(): string
+    {
+        $request = $this->requestStack->getMasterRequest() instanceof Request ? $this->requestStack->getMasterRequest() : null;
+
+        $requestCorrelationId = $request ? $request->headers->get(self::HEADER_KEY) : null;
+
+        return $requestCorrelationId ?? Uuid::uuid4()->toString();
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+}
