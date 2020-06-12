5150a4088ed524f5fb6173e74ba761ac89a049dc
Uday.Chitturi <uday.chitturi@smartbox.com>
2020-06-12 15:44:46 +0100

    Added all EAI Broadcasts tests to R2D2

--- tests/LoadTests/end2EndTests.py
+++ tests/LoadTests/end2EndTests.py
@@ -7,9 +7,11 @@ import datetime
 import os
 
 expected_time = 0.3
+partnerId = random.randint(1,8000000)
+boxProductId = random.randint(1,800000)
+experienceProductId = random.randint(1,800000)
 
 def broadcast_partner(self):
-   partnerId = random.randint(1,8000000)
    headers = {'content-type': 'application/json','Authorization':'Basic ZWFpOmVhaQ=='}
    response = self.client.post("/broadcast-listener/partner",data= json.dumps({
    "id": partnerId,
@@ -26,8 +28,110 @@ def broadcast_partner(self):
    except AssertionError as err:
        print(err)
 
+def broadcast_product_box(self):
+   headers = {'content-type': 'application/json','Authorization':'Basic ZWFpOmVhaQ=='}
+   response = self.client.post("/broadcast-listener/product",data= json.dumps({
+   "id": boxProductId,
+   "status": "inactive",
+   "currencyCode": "EUR",
+   "isSellable" : False,
+   "isReservable" : False,
+   "type": "mev",
+   "partner":{
+     "id":partnerId
+    },
+   "sellableBrand" : {
+     "code":"SBX"
+    },
+   "sellableCountry":{
+     "code":"FR"
+    },
+   "listPrice": {
+     "currencyCode": "EUR",
+     "amount" : 100
+     }
+   }),
+   headers=headers,
+   name = "Broadcast Product Box to R2D2")
+   try:
+       assert response.elapsed.total_seconds() < expected_time, "Request Broadcast Product Type Box took %r which is more than %r seconds" % (response.elapsed.total_seconds(), expected_time)
+       print ("Response code", response.status_code)
+       assert response.status_code == 202
+   except AssertionError as err:
+       print(err)
+
+def broadcast_product_experience(self):
+   headers = {'content-type': 'application/json','Authorization':'Basic ZWFpOmVhaQ=='}
+   response = self.client.post("/broadcast-listener/product",data= json.dumps({
+   "id": experienceProductId,
+   "status": "inactive",
+   "currencyCode": "EUR",
+   "isSellable" : False,
+   "isReservable" : False,
+   "type": "experience",
+   "partner":{
+     "id":partnerId
+    },
+   "sellableBrand" : {
+     "code":"SBX"
+    },
+   "sellableCountry":{
+     "code":"FR"
+    },
+   "listPrice": {
+     "currencyCode": "EUR",
+     "amount" : 100
+     }
+   }),
+   headers=headers,
+   name = "Broadcast Product Experience to R2D2")
+   try:
+       assert response.elapsed.total_seconds() < expected_time, "Request Broadcast Product Experience type took %r which is more than %r seconds" % (response.elapsed.total_seconds(), expected_time)
+       print ("Response code", response.status_code)
+       assert response.status_code == 202
+   except AssertionError as err:
+       print(err)
+
+def broadcast_product_relationship(self):
+   headers = {'content-type': 'application/json','Authorization':'Basic ZWFpOmVhaQ=='}
+   response = self.client.post("/broadcast-listener/product-relationship",data= json.dumps({
+   "parentProduct": boxProductId,
+   "childProduct" : experienceProductId,
+   "relationshipType" : "Box-Experience",
+   "isEnabled" : False
+   }),
+   headers=headers,
+   name = "Broadcast Box Experience Relationship to R2D2")
+   try:
+       assert response.elapsed.total_seconds() < expected_time, "Request Broadcast Box Experience took %r which is more than %r seconds" % (response.elapsed.total_seconds(), expected_time)
+       print ("Response code", response.status_code)
+       assert response.status_code == 202
+   except AssertionError as err:
+       print(err)
+
+def broadcast_price_relationship(self):
+   headers = {'content-type': 'application/json','Authorization':'Basic ZWFpOmVhaQ=='}
+   response = self.client.post("/broadcast-listener/price-information",data= json.dumps({
+   "product": {
+     "id":experienceProductId
+   },
+   "averageValue" : {
+     "amount" : 100,
+     "currencyCode" : "EUR"
+   },
+   "averageCommissionType" : "percentage",
+   "averageCommission" : 10
+   }),
+   headers=headers,
+   name = "Broadcast Price Information to R2D2")
+   try:
+       assert response.elapsed.total_seconds() < expected_time, "Request Broadcast Price Information took %r which is more than %r seconds" % (response.elapsed.total_seconds(), expected_time)
+       print ("Response code", response.status_code)
+       assert response.status_code == 202
+   except AssertionError as err:
+       print(err)
 
 class WebsiteUser(HttpUser):
-    tasks = {broadcast_partner: 1}
+    tasks = {broadcast_partner: 1,broadcast_product_box: 2, broadcast_product_experience: 2, broadcast_product_relationship: 1, broadcast_price_relationship: 1}
     min_wait = 5000
     max_wait = 9000
