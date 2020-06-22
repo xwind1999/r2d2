e5d0af94217b234d5c601f7db19e2f3e7c45aaa4
Uday.Chitturi <uday.chitturi@smartbox.com>
2020-06-22 11:12:18 +0100

    Testing Locust reporting

---
+++ tests/LoadTests/reportFile/graph.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+df = pd.read_csv('/home/udaychitturi/Documents/project_payment.py_stats.csv')
+
+fig = make_subplots(rows=1, cols=1)
+fig.add_trace(
+    go.Scatter(x = df['Name'], y = df['Average Response Time'], name='Average Response Time'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(x = df['Name'], y = df['Median Response Time'], name='Median Response Time'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(x = df['Name'], y = df['Min Response Time'], name='Min Response Time'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(x = df['Name'], y = df['Max Response Time'], name='Max Response Time'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(x = df['Name'], y = df['Request Count'], name='Total Request Count'),
+    row=1, col=1
+)
+
+fig.update_layout(height=1000, width=1500, xaxis_title="Type of Request",yaxis_title="Response in s",title_text="Locust Performance Results")
+fig.write_html("/home/udaychitturi/Documents/pythongraphs/result.html")
+fig.show()
