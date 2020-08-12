d2620333af41e1854c8edcbff6027f2241455504
Joao Jacome <joao.jacome@smartbox.com>
2020-08-12 18:18:35 +0100

    Fixed request validation for room availability and price

--- src/Contract/Request/BroadcastListener/RoomAvailabilityRequestList.php
+++ src/Contract/Request/BroadcastListener/RoomAvailabilityRequestList.php
@@ -7,11 +7,14 @@ namespace App\Contract\Request\BroadcastListener;
 use App\Helper\Request\RequestBodyInterface;
 use App\Helper\Request\ValidatableRequest;
 use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RoomAvailabilityRequestList implements ValidatableRequest, RequestBodyInterface
 {
     /**
      * @var RoomAvailabilityRequest[]
+     *
+     * @Assert\Valid
      *
      * @JMS\Type("array<App\Contract\Request\BroadcastListener\RoomAvailabilityRequest>")
      * @JMS\SerializedName("items")
--- src/Contract/Request/BroadcastListener/RoomPriceRequestList.php
+++ src/Contract/Request/BroadcastListener/RoomPriceRequestList.php
@@ -7,11 +7,14 @@ namespace App\Contract\Request\BroadcastListener;
 use App\Helper\Request\RequestBodyInterface;
 use App\Helper\Request\ValidatableRequest;
 use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RoomPriceRequestList implements ValidatableRequest, RequestBodyInterface
 {
     /**
      * @var RoomPriceRequest[]
+     *
+     * @Assert\Valid
      *
      * @JMS\Type("array<App\Contract\Request\BroadcastListener\RoomPriceRequest>")
      * @JMS\SerializedName("items")
