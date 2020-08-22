4e7e41b0391deacba0fb32679e7f16247d23883d
Joao Jacome <joao.jacome@smartbox.com>
2020-08-22 11:22:23 +0100

    Improved docker-compose for devint

--- bin/start-command.sh
+++ bin/start-command.sh
@@ -7,6 +7,8 @@ ln -s /etc/sv/sym /service/sym
 # defining a time limit for the command between 250 and 370 seconds, so the commands won't stop/start all at once
 RANDOM_DURATION=$((250 + RANDOM % 120))
 
+export APP_NAME=jarvis-r2d2-worker
+export LOG_FILE=r2d2-worker.log
 export COMMAND="/app/bin/console messenger:consume ${QUEUES} --time-limit=${RANDOM_DURATION}"
 
 /app/bin/warm-up.sh
