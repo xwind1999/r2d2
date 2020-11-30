073fc518050886a80d5186c7056c0291fdbc2e3b
Joao Jacome <joao.jacome@smartbox.com>
2020-11-30 18:16:54 +0000

    [R2D2-430] Dependency bump and composer 2

--- src/Exception/Booking/MisconfiguredExperiencePriceException.php
+++ src/Exception/Booking/MisconfiguredExperiencePriceException.php
@@ -8,6 +8,6 @@ use App\Exception\Http\UnprocessableEntityException;
 
 class MisconfiguredExperiencePriceException extends UnprocessableEntityException
 {
-    protected const MESSAGE = 'The experience price must be a number greater than zero';
+    protected const MESSAGE = 'Misconfigured experience price';
     protected const CODE = 1300006;
 }
